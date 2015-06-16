e2696c6d5aa7107432ca06074b711a6028dc37b2
Alvaro Valdebenito <avaldebe@gmail.com>
2015-06-16 23:40:50 +0200

    v1

--- init.lua
+++ init.lua
@@ -1,21 +1,34 @@
+local blinkD0,keyD0 -- blink D0 (gpio16) until setup
+blinkD0=function(start)
+  local D0=0
+  gpio.mode(D0,gpio.OUTPUT)
+  gpio.write(D0,0)
+  tmr.alarm(1,100,0,function()
+    gpio.write(D0,1)
+    gpio.mode(D0,gpio.INPUT)
+    keyD0=gpio.read(D0) -- stop on KEY_USER press
+    if keyD0==0 then
+      gpio.write(D0,1)
+      return
+      end
+    if start then
+      tmr.alarm(1,400,0,function() blinkD0(start) end)
+      end
+    end)
+  end
 -- https://bigdanzblog.wordpress.com/2015/04/24/esp8266-nodemcu-interrupting-init-lua-during-boot/
+-- modified for KEY_USER press
+blinkD0(true)
 tmr.alarm(0,1000,0,function()   -- after 1s
-  print('Press ENTER console mode')
-  local console=false
-  uart.on("data","\r",function(data)
-    console=true 
-    uart.on("data") 
-    end,0)
-  tmr.alarm(0,5000,0,function() -- after 5s
-    uart.on("data") 
-    if console==true then
+  print('Press KEY_USER for console mode')
+  tmr.alarm(0,1000,0,function() -- after 1s
+    if keyD0==0 then
       print('Console mode')
       return
       end
     -- otherwise, start up
+    blinkD0(false)
     print('Start WiFi')
-    dofile('wifi_init.lua')
+    dofile('init_wifi.lua')
     end)
   end)
-
-
--- wifi_init.lua
+++ wifi_init.lua
@@ -1,4 +1,25 @@
 wifi.setmode(wifi.STATION)
+local function listap(t)
+  local pass = require('wifi_ssid')  -- pass[ssid]  
+  for ssid,v in pairs(t) do
+    if pass[ssid] and wifi.sta.status()~=5 then
+      print("Loggin to: "..ssid)
+      wifi.sta.config(ssid,pass[ssid])
+      local n=5
+      repeat
+        tmr.delay(3000) -- 3 ms
+        n=n-1
+      until n<1 or wifi.sta.status()~=1
+    end
+    if pass[ssid] and wifi.sta.status()==5 then
+      print("Logged to: "..ssid)
+      print(""..wifi.sta.getip())
+    end
+  end
+end
+wifi.sta.getap(listap)
+
+wifi.setmode(wifi.STATION)
 local stat
 stat=function(n)
 if (n>0) and (wifi.sta.status()~=5) then
