ba11d7db16271639d3264115dc5dba7f648429dd
Alvaro Valdebenito <avaldebe@gmail.com>
2015-06-25 23:33:20 +0200

    remove DHT22

--- lua_modules/met.lua
+++ lua_modules/met.lua
@@ -21,18 +21,7 @@ function M.read(verbose)
     M.tostring('bmp085')
   end
 
-  require('dht22').read(1)
-  h,t = dht22.getHumidity(),dht22.getTemperature()
---dht22,package.loaded.dht22 = nil,nil -- release memory
-
-  M.h = h and ('%.1f'):format(h/10) or M.h
-  M.t = h and ('%.1f'):format(t/10) or M.t
-  if verbose then
-    M.tostring('dht22')
-  end
-
---require('dht22').read(2)
-  dht22.read(2)
+  require('dht22').read(2)
   h,t = dht22.getHumidity(),dht22.getTemperature()
   dht22,package.loaded.dht22 = nil,nil -- release memory
 
@@ -42,7 +31,7 @@ function M.read(verbose)
     M.tostring('am2321')
   end
 
-  mod,p,t,h = nil,nil,nil,nil -- release memory
+  p,t,h = nil,nil,nil -- release memory
   return M.t,M.h,M.p
 end
 
