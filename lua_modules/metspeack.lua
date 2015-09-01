8ba1fd22c39fdaa8acc82f2d055a06d766807b7c
Alvaro Valdebenito <avaldebe@gmail.com>
2015-09-01 11:04:50 +0200

    fix premature disconnection

--- lua_modules/metspeack.lua
+++ lua_modules/metspeack.lua
@@ -8,12 +8,18 @@ wifi_init,package.loaded.wifi_init=nil,nil
 local api=require('keys').api
 api.update={
   "GET /update?key={put}&status={stat}&field1={f1}&field2={f2}&field3={f3} HTTP/1.1\r\n",
-  "Host: {url}\r\n","Accept: */*\r\n",
-  "User-Agent: Mozilla/4.0 (compatible; esp8266 Lua; Windows NT 5.1)\r\n","\r\n"
+  "Host: {url}\r\nAccept: */*\r\n\r\n",
 }
 
+--require('met').init(5,6,false) -- sda,scl,lowHeap
 gpio.mode(0,gpio.OUTPUT)
-local function speack()
+api.last=tmr.now()
+local function speak()
+  if (tmr.now()-api.last<5e6) or -- 5s since last (debounce/long press)
+     (api.sent~=nil) then        -- already sending data
+    return
+  end
+  api.sent=false
   print('Sleep mode: NONE_SLEEP')
   wifi.sleeptype(wifi.NONE_SLEEP)
   if wifi.sta.status()~=5 then
@@ -21,7 +27,9 @@ local function speack()
     node.restart() -- connection failed
   end
   print("Read sensors")
-  require('met').init(3,4,true) -- sda,scl,lowHeap
+  if met==nil then
+    require('met').init(5,6,true) -- sda,scl,lowHeap
+  end
   met.read(true) -- verbose
   api.f1,api.f2,api.f3=met.t,met.h,met.p
   met,package.loaded.met=nil,nil
@@ -30,40 +38,44 @@ local function speack()
   api.stat=('uptime=%s, heap=%d'):format(uptime,node.heap())
   print('  '..api.stat)
 
-  local conn=net.createConnection(net.TCP,0)
-  conn:on("receive",   function(conn,payload)
-    print(payload:find("Status: 200 OK") and "  Posted OK" or "  Recieved: "..payload)
+  local sk=net.createConnection(net.TCP,0)
+  sk:on("receive",   function(conn,payload)
+--  print(("  Recieved: '%s'"):format(payload))
+    if payload==nil or payload:find("Status: 200 OK") then
+      print("  Posted OK")
+    end
   end)
-  conn:on("connection",function(conn,payload)
+  sk:on("connection",function(conn)
     print("  Connected")
     gpio.write(0,0)
     print("  Send data")
     local i=0
     for i=1,#api.update do
-    --print(api.update[i]:gsub("{(.-)}",api))
       conn:send(api.update[i]:gsub("{(.-)}",api))
     end
+    api.sent=true
   end)
-  conn:on("sent",function(conn,payload)
-    print("  Data sent")
-    conn:close()
+  sk:on("sent",function(conn)
+    if api.sent then
+      print("  Data sent")
+    end
   end)
-  conn:on("disconnection",function(conn,payload)
+  sk:on("disconnection",function(conn)
     print("  Disconnected")
     gpio.write(0,1)
     print('Sleep mode: MODEM_SLEEP')
     wifi.sleeptype(wifi.MODEM_SLEEP)
+    api.last=tmr.now()
+    api.sent=nil
     collectgarbage()
   end)
   print('Send sensor data to '..api.url)
-  conn:connect(80,api.url)
+  sk:connect(80,api.url)
 end
 
 print(('Send data every %s min'):format(api.freq))
-tmr.alarm(0,api.freq*60e3,1,function() speack() end)
+tmr.alarm(0,api.freq*60e3,1,function() speak() end)
 
 print('Press KEY_FLASH to send NOW')
 gpio.mode(3,gpio.INT)
-gpio.trig(3,"low",function(state) speack() end)
-
-
+gpio.trig(3,"low",function(state) speak() end)
--- lua_modules/upload.sh
+++ lua_modules/upload.sh
@@ -4,15 +4,16 @@
 while (($#)); do
   PORT=`ls /dev/ttyUSB? /dev/rfcomm? 2>/dev/null`
   opt=$1
+  trap "exit" SIGHUP SIGINT SIGTERM
   case $opt in
   wipe)
     luatool.py -p $PORT -rwl;;
   bmp180|dht22|am2321)
     luatool.py -p $PORT -rcf $opt.lua;;
   keys|keys.*|my_conf|my_conf.*)
-    luatool.py -p $PORT -cf my_conf.lua   -t keys.lua;;
+    luatool.py -p $PORT -cf my_conf.lua -t keys.lua;;
   metspeack|metspeack.*)
-    luatool.py -p $PORT -cf metspeack.lua -t app.lua;;
+    luatool.py -p $PORT -rcf metspeack.lua -t app.lua;;
   wifi_init|met)
     luatool.py -p $PORT -cf $opt.lua;;
   init|init.lua)
@@ -20,5 +21,6 @@ while (($#)); do
   *)
     luatool.py -p $PORT -cf ${opt%.*}.lua;;
   esac && shift
+  trap - SIGHUP SIGINT SIGTERM
 done
 
