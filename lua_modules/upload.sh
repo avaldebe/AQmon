089a38fafff13d745b5d99ae35222baa807b2e1e
Alvaro Valdebenito <avaldebe@gmail.com>
2015-09-07 14:31:35 +0200

    debug re-commection

--- lua_modules/upload.sh
+++ lua_modules/upload.sh
@@ -13,8 +13,8 @@ while (($#)); do
   keys|keys.*|my_conf|my_conf.*)
     luatool.py -p $PORT -cf my_conf.lua -t keys.lua;;
   metspeack|metspeack.*)
-    luatool.py -p $PORT -rf metspeack.lua -t app.lua;;
-  wifi_init) #|met)
+    luatool.py -p $PORT -rcf metspeack.lua -t app.lua;;
+  wifi_init|met)
     luatool.py -p $PORT -cf $opt.lua;;
   init|init.lua)
     luatool.py -p $PORT -rf ${opt%.*}.lua;;
