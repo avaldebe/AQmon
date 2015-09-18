f65352bb6d5e225b808cdce38dce271e7319cad5
Alvaro Valdebenito <avaldebe@gmail.com>
2015-09-18 23:16:01 +0200

    very low heap

--- lua_modules/AQmon.lua
+++ lua_modules/AQmon.lua
@@ -8,14 +8,17 @@ Written by Álvaro Valdebenito.
 MIT license, http://opensource.org/licenses/MIT
 ]]
 
--- HW pin assignments
+--[[ HW pin assignment ]]
+
 local pin={ledR=1,ledG=2,ledB=4,sda=5,scl=6}
+
+--[[ Local functions ]]
 
 -- LED status indicator
 local status=dofile('rgbLED.lc')(500,pin.ledR,pin.ledG,pin.ledB,
   {alert='320000',alert='010000',normal='000100',iddle='000001'})
---local function status(msg) end
 status('normal')
+-- low heap(?) alternative: local status=print
 
 print('Start WiFi')
 require('wifi_init').connect(wifi.STATION)
@@ -28,13 +31,13 @@ status('iddle')
 
 local api=require('keys').api
 gpio.mode(0,gpio.OUTPUT)
-function api.sendData()
+function api:sendData()
   status('normal')
-  if api.sent~=nil then -- already sending data
+  if self.sent~=nil then -- already sending data
     status('iddle')
     return
   end
-  api.sent=false
+  self.sent=false
 
   if wifi.sta.status()~=5 then
     print('WiFi wakeup')
@@ -44,16 +47,13 @@ function api.sendData()
   wifi.sleeptype(wifi.NONE_SLEEP)
 
   local sk=net.createConnection(net.TCP,0)
-  sk:on('receive',   function(conn,payload)
-    status('alert')
-    assert(conn~=nil,'socket:on(receive) stale socket')
-    status('normal')
-  --print(('  Recieved: "%s"'):format(payload))
-    if payload:find('Status: 200 OK') then
-      print('  Posted OK')
-    end
-    status('iddle')
-  end)
+--[[Expected sequence of events:
+    sk:connect(...)
+    sk:on('connection')
+      sk:send(...)
+    sk:on('sent')
+    sk:on('receive')
+    sk:on('disconnection')]]
   sk:on('connection',function(conn)
     status('alert')
     assert(conn~=nil,'socket:on(connection) stale socket')
@@ -74,8 +74,18 @@ function api.sendData()
     assert(conn~=nil,'socket:on(sent) stale socket')
     status('normal')
     print('  Data sent')
-    api.sent=true
+    self.sent=true
   --conn:close()
+    status('iddle')
+  end)
+  sk:on('receive',   function(conn,payload)
+    status('alert')
+    assert(conn~=nil,'socket:on(receive) stale socket')
+    status('normal')
+  --print(('  Recieved: "%s"'):format(payload))
+    if payload:find('Status: 200 OK') then
+      print('  Posted OK')
+    end
     status('iddle')
   end)
   sk:on('disconnection',function(conn)
@@ -88,34 +98,39 @@ function api.sendData()
     print('WiFi sleep')
     wifi.sleeptype(wifi.MODEM_SLEEP)
     wifi.sta.disconnect()
-    api.sent=nil
+    self.sent=nil
+    self.last=tmr.now()
     status('iddle')
   end)
-  print(('Send data to %s.'):format(api.url))
-  sk:connect(80,api.url)
+  print(('Send data to %s.'):format(self.url))
+  sk:connect(80,self.url)
 end
 
-api.last=tmr.now()
-require('sensors').init(pin.sda,pin.scl,false) -- sda,scl,lowHeap
 local function speak()
-  if (tmr.now()-api.last<5e6) then -- 5s since last (debounce/long press)
+  if api.last and (tmr.now()-api.last<5e6) then -- 5s since last (debounce/long press)
     return
   end
   local lowHeap=true
   print('Read data')
   require('sensors').init(pin.sda,pin.scl,lowHeap) -- sda,scl,lowHeap
   sensors.read(true)                   -- verbose
-  api.path=sensors.format('update?key={put}&status=uptime={upTime},heap={heap}'
+  api.path=sensors.format('status=uptime={upTime},heap={heap}'
   ..'&field1={t}&field2={h}&field3={p}&field4={pm01}&field5={pm25}&field6={pm10}',
-    true):gsub('{(.-)}',api) -- remove spaces
+    true) -- remove spaces
 -- release memory
   if lowHeap then
     sensors,package.loaded.sensors=nil,nil
     collectgarbage()
   end
-  api.sendData()
+  api.path=('update?key={put}&{path}'):gsub('{(.-)}',api)
+  api:sendData()
   api.last=tmr.now()
 end
+
+--[[ Run code ]]
+
+-- start PM sensor data collection
+require('sensors').init(pin.sda,pin.scl,false) -- sda,scl,lowHeap
 
 --api.freq=1 -- debug
 print(('Send data every %s min'):format(api.freq))
