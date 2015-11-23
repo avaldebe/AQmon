9285681c795b7939a001e4aadbd66cd14d3bf5b0
Alvaro Valdebenito <avaldebe@gmail.com>
2015-11-23 10:58:58 +0100

    partial rewrite M.format

--- lua_modules/sensor_hub.lua
+++ lua_modules/sensor_hub.lua
@@ -14,39 +14,35 @@ local moduleName = ...
 local M = {}
 _G[moduleName] = M
 
--- Output variables
-M.p,M.h,M.t='null','null','null' -- atm.pressure,rel.humidity,teperature
-M.pm01,M.pm25,M.pm10='null','null','null' -- particulate matter (PM)
-
 -- Format module outputs
-function M.format(message,squeese,t,h,p,pm01,pm25,pm10)
--- padd initial values for csv/column output
-  if M.p   =='null' then M.p   =('%7s'):format(M.p) end
-  if M.h   =='null' then M.h   =('%5s'):format(M.h) end
-  if M.t   =='null' then M.t   =('%5s'):format(M.t) end
-  if M.pm01=='null' then M.pm01=('%4s'):format(M.pm01) end
-  if M.pm25=='null' then M.pm25=('%4s'):format(M.pm25) end
-  if M.pm10=='null' then M.pm10=('%4s'):format(M.pm10) end
-
+function M.format(tab,message,squeese)
+  local k,v
+  for k,v in pairs(tab)
 -- formatted output (w/padding) from integer values
-  if type(p)=='number' then
-    M.p=('%6d'):format(p) -- p/100 --> %7.2f
-    M.p=('%4s.%2s'):format(M.p:sub(1,4),M.p:sub(5))
+    if type(v)~='number' then
+      if k=='pm01' or k=='pm25' or k=='pm10' then
+        M[k]=('%4d'):format(v)
+      elseif k=='t' or k=='h' then
+        v=('%4d'):format(v) -- t/10|h/10 --> %5.1f
+        M[k]=('%3s.%1s'):format(v:sub(1,3),v:sub(4))
+      elseif k=='p' then
+        v=('%6d'):format(v) -- p/100 --> %7.2f
+        M[k]=('%4s.%2s'):format(v:sub(1,4),v:sub(5))
+      end
+-- formatted output (w/padding) default values ('null')
+    elseif type(v)~='nil' or type(v)~='string' then
+      if k=='pm01' or k=='pm25' or k=='pm10' then
+        M[k]=('%4s'):format(v and v or 'null')
+      elseif k=='t' or k=='h' then
+        M[k]=('%5s'):format('null')
+      elseif k=='p' then
+        M[k]=('%7s'):format('null')
+      end
+    end
   end
-  if type(h)=='number' then
-    M.h=('%4d'):format(h) -- h/10  --> %5.1f
-    M.h=('%3s.%1s'):format(M.h:sub(1,3),M.h:sub(4))
-  end
-  if type(t)=='number' then
-    M.t=('%4d'):format(t) -- t/10  --> %5.1f
-    M.t=('%3s.%1s'):format(M.t:sub(1,3),M.t:sub(4))
-  end
-  if type(pm01)=='number' then M.pm01=('%4d'):format(pm01) end
-  if type(pm25)=='number' then M.pm25=('%4d'):format(pm25) end
-  if type(pm10)=='number' then M.pm10=('%4d'):format(pm10) end
 
 -- process message for csv/column output
-  if type(message)=='string' then
+  if type(message)=='string' and message~='' then
     local uptime=tmr.time()
     M.upTime=('%02d:%02d:%02d:%02d'):format(uptime/86400, -- days:
               uptime%86400/3600,uptime%3600/60,uptime%60) -- hh:mm:ss
@@ -60,6 +56,9 @@ function M.format(message,squeese,t,h,p,pm01,pm25,pm10)
     return payload
   end
 end
+
+-- Output variables (padded for csv/column output)
+M.format({p=nil,h=nil,t=nil,pm01=nil,pm25=nil,pm10=nil})
 
 local cleanup=false     -- release modules after use
 local persistence=false -- use last values when read fails
@@ -85,38 +84,39 @@ function M.read(verbose)
     return
   end
 
-  local p,t,h,pm01,pm25,pm10
-
   if not persistence then
-    M.p,M.t,M.h='null','null','null'
-    M.pm01,M.pm25,M.pm10='null','null','null'
+    -- Output variables (padded for csv/column output)
+    M.format({p=nil,h=nil,t=nil,pm01=nil,pm25=nil,pm10=nil})
   end
+  local vars={}
   local payload='{upTime}  %-7s:{t}[C],{h}[%%],{p}[hPa],{pm01},{pm25},{pm10}[ug/m3]  heap:{heap}'
 
   require('i2d').init(nil,SDA,SCL)
   require('bmp180').init(SDA,SCL)
   bmp180.read(0)   -- 0:low power .. 3:oversample
-  p,t = bmp180.pressure,bmp180.temperature
+  vars={p=bmp180.pressure,t=bmp180.temperature}
   if cleanup then  -- release memory
     bmp180,package.loaded.bmp180 = nil,nil
     i2d,package.loaded.i2d = nil,nil
   end
   if verbose then
-    print(M.format(payload:format('bmp085'),false,t,h,p,pm01,pm25,pm10))
-    p,t = nil,nil -- release variables to avoid re-formatting
+    print(M.format(vars,payload:format('am2321'),false))
+  else
+    M.format(vars)
   end
 
   require('i2d').init(nil,SDA,SCL)
   require('am2321').init(SDA,SCL)
   am2321.read()
-  h,t = am2321.humidity,am2321.temperature
+  vars={h=am2321.humidity,t=am2321.temperature}
   if cleanup then  -- release memory
     am2321,package.loaded.am2321=nil,nil
     i2d,package.loaded.i2d = nil,nil
   end
   if verbose then
-    print(M.format(payload:format('am2321'),false,t,h,p,pm01,pm25,pm10))
-    h,t = nil,nil -- release variables to avoid re-formatting
+    print(M.format(vars,payload:format('bmp085'),false))
+  else
+    M.format(vars}
   end
 
   require('pms3003').init()
