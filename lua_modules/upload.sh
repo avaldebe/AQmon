5263fbe718513ab768d6157a0afc72bd334743c5
Alvaro Valdebenito <avaldebe@gmail.com>
2015-12-03 13:31:24 +0100

    do not reboot after compiling seneor modules

--- lua_modules/upload.sh
+++ lua_modules/upload.sh
@@ -18,7 +18,7 @@ while (($#)); do
   wipe)
     luatool.py -p $PORT -rw;;
   bmp180|am2321|pms3003)        # sensor modules
-    luatool.py -p $PORT -rcf $opt.lua;;
+    luatool.py -p $PORT -cf $opt.lua;;
   hub|hub.*|*_hub|*_hub.*)      # sensor hub module
     luatool.py -p $PORT -rcf ${opt%.*}.lua -t sensors.lua;;
   keys|wifi_connect|sendData|rgbLED|hueLED)
