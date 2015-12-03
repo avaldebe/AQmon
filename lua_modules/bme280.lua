37db99bc6983c264e98bc0aa9641b4d18d35ece5
Alvaro Valdebenito <avaldebe@gmail.com>
2015-12-04 00:16:37 +0100

    compensation, not yet ready

--- lua_modules/bme280.lua
+++ lua_modules/bme280.lua
@@ -7,9 +7,12 @@ Written by Álvaro Valdebenito,
   based on:
   - bme280.lua by WG
     https://github.com/wogum/esp12
-  - bme280.py  by Kieran Brownlees
+  - bme280.py by Kieran Brownlees
     https://github.com/kbrownlees/bme280
-  -
+  - Adafruit_BME280.py by adafruit
+    https://github.com/adafruit/Adafruit_Python_BME280
+  - BME280_driver by BoschSensortec
+    https://github.com/BoschSensortec/BME280_drive
 
 MIT license, http://opensource.org/licenses/MIT
 ]]
@@ -148,13 +151,68 @@ function M.read(oss)
   i2c.address(id,ADDR,i2c.RECEIVER)
   local c = i2c.read(id,8) -- p:3byte,t:3byte,h:byte
   i2c.stop(id)
--- unpack DATA
-  local p=c:byte(1)*4096+c:byte(2)*16+c:byte(3)/16
-  local t=c:byte(4)*4096+c:byte(5)*16+c:byte(6)/16
-  local h=c:byte(7)* 256+c:byte(8)
+-- unpack RAW DATA
+  local p,t,h
+  p=c:byte(1)*4096+c:byte(2)*16+c:byte(3)/16  -- uncompensated pressure
+  t=c:byte(4)*4096+c:byte(5)*16+c:byte(6)/16  -- uncompensated temperature
+  h=c:byte(7)* 256+c:byte(8)                  -- uncompensated humidity
+
+--[[ Temperature: Adapted from bme280_compensate_temperature_int32.
+  Calculate actual temperature from uncompensated temperature.
+  Returns the value in 0.01 degree Centigrade (DegC),
+  an output value of "5123" equals 51.23 DegC. ]]
+  local v1,v2,tfine
+  t  = t/8 - cal.T1*2
+  v1 = bit.rshift(t*cal.T2,11)
+  v2 = bit.rshift((t/2)*(t/2),12)
+  tfine = v1 + bit.rshift(v2*cal.T3,14)
+  t = bit.rshift(tfine*5 + 128,8)
+
+--[[ Pressure: Adapted from bme280_compensate_pressure_int32.
+  Calculate actual pressure from uncompensated pressure.
+  Returns the value in Pascal (Pa),
+  and output value of "96386" equals 96386 Pa = 963.86 hPa. ]]
+  v1 = tfine/2 - 64000
+  v2 = bit.rshift((v1/4)*(v1/4),11)*cal.P6
+  v2 = v2 + v1*cal.P5*2
+  v2 = v2/4 + bit.lshift(cal.P4,16)
+  v2 = bit.rshift(v2,12)
+  v1 = cal.P3*bit.rshift((v1/4)*(v1/4),13)/8
+     + bit.rshift(cal.P2*v1/2,18)
+  v1 = bit.rshift((32768 + v1)*cal.P1,15)
+  if v1==0 then
+    p = nil
+  else
+    p = (1048576 - p - v2)*3125
+    p = p*2/v1
+    v1 = bit.rshift((p/8)*(p/8),13)
+    v1 = bit.rshift(v1*cal.P9,12)
+    v2 = bit.rshift(p/4*cal.P8,13)
+    p = p + bit.rshift(v1 + v2 + cal.P7,4)
+  end
+
+
+--[[ Humidity: Adapted from bme280_compensate_humidity_int32.
+  Calculte actual humidity from uncompensated humidity.
+  Returns the value in %rH as unsigned 32bit integer
+  in Q22.10 format(22 integer 10 fractional bits).
+  An output value of 42313 represents 42313 / 1024 = 41.321 %rH ]]
+  v1 = tfine - 76800
+  v2 = v1 * cal.H6 / 1024
+  v1 = h * 16384 - cal.H4 * 1048576 - cal.H5 * v1 + 16384
+  v2 = ((v2 * v2 + 32768) / 1024 + 2097152) * cal.H2 + 8192
+  v1 = (v1/32768) * (v2/16384)
+  v2 = (v1/32768) * (v1/32768)
+  v1 = v1 - v2/128 * H1 / 16
+  if v1 < 0 then
+    v1 = 0
+  elseif v1 > 419430400 then
+    v1 = 419430400
+  end
+  h = v1 / 4096
 
 -- expose results
-  M.temperature=p -- integer value of temperature [10*C]
-  M.pressure   =t -- integer value of preassure [100*hPa]
-  M.humidity   =h -- integer value of relative humidity [10*%]
+  M.temperature=t -- integer value of temperature [0.01 C]
+  M.pressure   =p -- integer value of preassure   [0.01 hPa]
+  M.humidity   =h -- integer value of rel.humidity[0.01 %]
 end
