592a54735d19b3ba03ddcfce868370c883361bff
Alvaro Valdebenito <avaldebe@gmail.com>
2015-12-25 00:21:43 +0100

    rewrite module

--- lua_modules/bme280.lua
+++ lua_modules/bme280.lua
@@ -36,11 +36,11 @@ local M={
 }
 _G[M.name]=M
 
--- BME280 have 2 possible addresses
+-- BME280 has 2 possible addresses
 local ADDR = {0x76,0x77}
 
 -- calibration coefficients
-local cal={} -- T1,..,T3,P1,..,P9,H1,..,H6
+local T,P,H={},{},{} -- T1,..,T3,P1,..,P9,H1,..,H6
 
 local function int16_t(uint,nbits)
 -- first negative number
@@ -130,7 +130,7 @@ function M.init(SDA,SCL,volatile,...)
   i2c.setup(0,SDA,SCL,i2c.SLOW)
 
 -- M.init suceeded after/when read calibration coeff.
-  init=(next(cal)~=nil)
+  init=(next(T)~=nil)and(next(P)~=nil)and(next(H)~=nil)
 
   if not init then
     local found,c
@@ -180,36 +180,36 @@ function M.init(SDA,SCL,volatile,...)
         end
       end
     -- unpack CALIBRATION: T1,..,T3,P1,..,P9,H1,..,H7
-      cal.T1=        c:byte( 1)+c:byte( 2)*256  -- 0x88,0x89; unsigned short
-      cal.T2=int16_t(c:byte( 3)+c:byte( 4)*256) -- 0x8A,0x8B; (signed) short
-      cal.T3=int16_t(c:byte( 5)+c:byte( 6)*256) -- 0x8C,0x8D; (signed) short
-      cal.P1=        c:byte( 7)+c:byte( 8)*256  -- 0x8E,0x8F; unsigned short
-      cal.P2=int16_t(c:byte( 9)+c:byte(10)*256) -- 0x90,0x91; (signed) short
-      cal.P3=int16_t(c:byte(11)+c:byte(12)*256) -- 0x92,0x93; (signed) short
-      cal.P4=int16_t(c:byte(13)+c:byte(14)*256) -- 0x94,0x95; (signed) short
-      cal.P5=int16_t(c:byte(15)+c:byte(16)*256) -- 0x96,0x97; (signed) short
-      cal.P6=int16_t(c:byte(17)+c:byte(18)*256) -- 0x98,0x99; (signed) short
-      cal.P7=int16_t(c:byte(19)+c:byte(20)*256) -- 0x9A,0x9B; (signed) short
-      cal.P8=int16_t(c:byte(21)+c:byte(22)*256) -- 0x9C,0x9D; (signed) short
-      cal.P9=int16_t(c:byte(23)+c:byte(24)*256) -- 0x9E,0x9F; (signed) short
-      cal.H1=        c:byte(25)                 -- 0xA1     ; unsigned char
-      cal.H2=int16_t(c:byte(26)+c:byte(27)*256) -- 0xE1,0xE2; (signed) short
-      cal.H3=        c:byte(28)                 -- 0xE3     ; unsigned char
-      cal.H4=bit.band(c:byte(30),0x0F)          -- 0xE5[3:0],...
-      cal.H4=int16_t(cal.H4+c:byte(29)*16)      --  ...,0xE4; (signed) short
-      cal.H5=bit.rshift(c:byte(30),4)           -- 0xE5[7:4],...
-      cal.H5=int16_t(cal.H5+c:byte(31)*16)      --  ...,0xE6; (signed) short
-      cal.H6=int16_t(c:byte(32),8)              -- 0xE7     ; (signed) char
+      T[1]=        c:byte( 1)+c:byte( 2)*256  -- 0x88,0x89; unsigned short
+      T[2]=int16_t(c:byte( 3)+c:byte( 4)*256) -- 0x8A,0x8B; (signed) short
+      T[3]=int16_t(c:byte( 5)+c:byte( 6)*256) -- 0x8C,0x8D; (signed) short
+      P[1]=        c:byte( 7)+c:byte( 8)*256  -- 0x8E,0x8F; unsigned short
+      P[2]=int16_t(c:byte( 9)+c:byte(10)*256) -- 0x90,0x91; (signed) short
+      P[3]=int16_t(c:byte(11)+c:byte(12)*256) -- 0x92,0x93; (signed) short
+      P[4]=int16_t(c:byte(13)+c:byte(14)*256) -- 0x94,0x95; (signed) short
+      P[5]=int16_t(c:byte(15)+c:byte(16)*256) -- 0x96,0x97; (signed) short
+      P[6]=int16_t(c:byte(17)+c:byte(18)*256) -- 0x98,0x99; (signed) short
+      P[7]=int16_t(c:byte(19)+c:byte(20)*256) -- 0x9A,0x9B; (signed) short
+      P[8]=int16_t(c:byte(21)+c:byte(22)*256) -- 0x9C,0x9D; (signed) short
+      P[9]=int16_t(c:byte(23)+c:byte(24)*256) -- 0x9E,0x9F; (signed) short
+      H[1]=        c:byte(25)                 -- 0xA1     ; unsigned char
+      H[2]=int16_t(c:byte(26)+c:byte(27)*256) -- 0xE1,0xE2; (signed) short
+      H[3]=        c:byte(28)                 -- 0xE3     ; unsigned char
+      H[4]=bit.band(c:byte(30),0x0F)          -- 0xE5[3:0],...
+      H[4]=int16_t(H[4]+c:byte(29)*16)      --  ...,0xE4; (signed) short
+      H[5]=bit.rshift(c:byte(30),4)           -- 0xE5[7:4],...
+      H[5]=int16_t(H[5]+c:byte(31)*16)      --  ...,0xE6; (signed) short
+      H[6]=int16_t(c:byte(32),8)              -- 0xE7     ; (signed) char
       c=nil
     end
     -- M.init suceeded
     init=found
   end
   if init and M.verbose==true then
-    print((('%s:\n--cal.%s.\n--cal.%s.\n--cal.%s.'):format(M.name,
-      'T[1:3]={T1},{T2},{T3}',
-      'P[1:7]={P1},{P2},{P3},{P4},{P5},{P6},{P7}',
-      'H[1:6]={H1},{H2},{H3},{H4},{H5},{H6}'):gsub('{(.-)}',cal)))
+    print(('%s: cal.coeff.\n--T={%s}.\n--P={%s}.\n--H={%s}.'):format(M.name,
+      (('{1},{2},{3}'):gsub('{(.-)}',T)),
+      (('{1},{2},{3},{4},{5},{6},{7}'):gsub('{(.-)}',P)),
+      (('{1},{2},{3},{4},{5},{6}'):gsub('{(.-)}',H))))
   end
 
 -- Sampling setup
@@ -244,10 +244,10 @@ function M.read(...)
   Returns the value in 0.01 degree Centigrade (DegC),
   an output value of "5123" equals 51.23 DegC. ]]
   local v1,v2,v3,tfine
-  t  = t/8 - cal.T1*2
-  v1 = bit.arshift(t*cal.T2,11)
-  v2 = bit.rshift(t*t,14)
-  tfine = v1 + bit.arshift(v2*cal.T3,14)
+  v1 = t/8 - T[1]*2
+  v2 = bit.arshift(v1*T[2],11)
+  v3 = bit.rshift((v1/2)*(v1/2),12)
+  tfine = v2 + bit.arshift(v3*T[3],14)
   t = bit.arshift(tfine*5 + 128,8)
   if M.verbose==true then
     print(('%s: tfine=%d.'):format(M.name,tfine))
@@ -257,27 +257,24 @@ function M.read(...)
   Calculate actual pressure from uncompensated pressure.
   Returns the value in Pascal (Pa),
   and output value of "96386" equals 96386 Pa = 963.86 hPa. ]]
-  v1 = tfine/2 - 64000
-  v2 = bit.rshift(v1*v1,15)
-  v3 = v2/4
-  v2 = v2*cal.P6 + v1*cal.P5*2
-  v3 = bit.arshift(v3*cal.P3/4+v1*cal.P2,19) + 32768
-  v1 = bit.rshift(v3*cal.P1,15)
+  v1 = tfine - 128000
+  v2 = bit.rshift(v1*v1,17)
+  v3 = bit.arshift(v2/4*P[3]/4+v1*P[2],19) + 32768
+  v2 = (v2*P[6] + v1*P[5])/4 + bit.lshift(P[4],16)
+  v1 = bit.rshift(v3*P[1],15)
   if v1==0 then -- p/0 will lua-panic
     p = nil
   else
-    v2 = v2/4 + bit.lshift(cal.P4,16)
-    v3 = bit.arshift(v2,12)
-    p = (1048576 - p - v3)*3125
+    p = (1048576 - p - bit.arshift(v2,12))*3125
     if p*2>0 then -- avoid overflow (signed) int32
       p = p*2/v1
     else
       p = p/v1*2
     end
     v1 = bit.rshift(p*p,19)
-    v2 = bit.arshift(v1*cal.P9,12)
-    v3 = bit.arshift(p*cal.P8,15)
-    p = p + bit.arshift(v2 + v3 + cal.P7,4)
+    v2 = bit.arshift(v1*P[9],12)
+    v3 = bit.arshift(p*P[8],15)
+    p = p + bit.arshift(v2 + v3 + P[7],4)
   end
 
 --[[ Humidity: Adapted from bme280_compensate_humidity_int32.
@@ -285,15 +282,15 @@ function M.read(...)
   Returns the value in 0.01 %rH.
   An output value of "4132.1" represents 41.321 %rH ]]
   v1 = tfine - 76800
-  v2 = bit.rshift(v1*cal.H6,10)
-  v3 = bit.rshift(v1*cal.H3,11) + 32768
-  v1 = bit.lshift(h,14) - bit.lshift(cal.H4,20) - cal.H5*v1 + 16384
+  v2 = bit.rshift(v1*H[6],10)
+  v3 = bit.rshift(v1*H[3],11) + 32768
+  v1 = bit.lshift(h,14) - bit.lshift(H[4],20) - H[5]*v1 + 16384
   v2 = bit.rshift(v2*v3,10) + 2097152
-  v3 = v2*cal.H2 + 8192
+  v3 = v2*H[2] + 8192
   v1 = bit.rshift(v1,15)*bit.rshift(v3,14)
   v2 = bit.rshift(v1,15)
   v3 = bit.rshift(v2*v2,7)
-  v1 = v1 - bit.rshift(v3*cal.H1,4)
+  v1 = v1 - bit.rshift(v3*H[1],4)
 -- v1 between 0 and 100*2^22
   if v1 < 0 then
     v1 = 0
